63aade61f7716ad0b5b53d14da8ccc4b370ffa39
Héctor <53621081+iamamitito@users.noreply.github.com>
2021-01-16 15:37:19 -0300

    refactoring (#9)

--- client/src/components/Charts/LineChart.vue
+++ client/src/components/Charts/LineChart.vue
@@ -3,21 +3,13 @@
 
   export default {
     extends: Line,
+    props: {
+      chartData: {
+        type: Object
+      }
+    },
     data () {
       return {
-        chartData: {
-          labels: ["01 Enero",	"02 Enero",	"03 Enero",	"04 Enero",	"05 Enero",	"06 Enero",	"07 Enero", "08 Enero", "09 Enero", "Hoy"],
-          datasets: [
-            {
-              label: 'Peso',
-              data: [85,	80,	74,	80,	72,	83,	70,	65,	62, 50],
-              fill: false,
-              borderColor: '#53E0BF',
-              backgroundColor: '#53E0BF',
-              borderWidth: 1
-            }
-          ]
-        },
         options: {
           scales: {
             yAxes: [{
--- client/src/components/profile/Profile.vue
+++ client/src/components/profile/Profile.vue
@@ -6,7 +6,7 @@
         </b-col>
         <b-col class="weighty-card">
             <h2>Mi progreso</h2>
-            <LineChart/>
+            <LineChart :chartData="chartData"></LineChart>
         </b-col>
     </b-row>
 </b-container>
@@ -29,8 +29,26 @@ export default {
       }
     },
     mounted() {
+        this.fillData();
     },
     methods: {
+        fillData() {
+            this.chartData = {
+                labels: ["01 Enero","02 Enero",	"03 Enero",	"04 Enero",	"05 Enero",	"06 Enero",	"07 Enero", "08 Enero", "09 Enero", "Hoy"],
+          datasets: [
+            {
+              label: 'Peso',
+              data: [85, 80, 74, 80, 72, 83, 70, 65, 62, 50],
+              fill: false,
+              borderColor: '#53E0BF',
+              backgroundColor: '#53E0BF',
+              borderWidth: 1
+            }
+          ]
+            }
+        }
+        
+
 }
 }
 </script>
